a1bbfc1e6f79b733d5a5abbaf70e98e90e515b12
Majd Al-shihabi <majd.al@gmail.com>
2020-02-27 17:39:08 +0200

    More sane deployment settings

--- backend/config/local_settings.py
+++ backend/config/local_settings.py
@@ -1,8 +1,9 @@
 # This file is exec'd from settings.py, so it has access to and can
 # modify all the variables in settings.py.
 import os
+from distutils.util import strtobool
 
-DEBUG = True
+DEBUG = bool(strtobool(os.environ.get('DJANGO_DEBUG', False)))
 
 # Make these unique, and don't share it with anybody.
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -26,4 +27,4 @@ DATABASES = {
 }
 
 # Allowed development hosts
-ALLOWED_HOSTS = ["*", "127.0.0.1", "0.0.0.0", "::1"]
+ALLOWED_HOSTS = [os.environ['PROJECT_URL'], ]
