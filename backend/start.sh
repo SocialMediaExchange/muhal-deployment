d604001b8cc3964661aaf4ec232215c52acec576
Majd Al-shihabi <majd.al@gmail.com>
2020-03-09 13:03:02 +0200

    nginx frontend for the platform that serves static content through named volume

---
+++ backend/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pipenv run python manage.py collectstatic --noinput
+pipenv run python manage.py migrate
+
+pipenv run gunicorn -b 0.0.0.0:8000 --log-file=/var/log/gunicorn.log muhal.wsgi
