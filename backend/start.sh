fbd6bfad68bdd55e34e439e07fc17c661a4da51f
Majd Al-shihabi <majd.al@gmail.com>
2020-06-16 19:12:35 +0300

    add client service

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pipenv run python manage.py compilemessages
-pipenv run python manage.py collectstatic --noinput
-pipenv run python manage.py migrate
+python manage.py compilemessages
+python manage.py collectstatic --noinput
+python manage.py migrate
 
-pipenv run gunicorn -b 0.0.0.0:8000 --log-file=/var/log/gunicorn.log muhal.wsgi
+gunicorn -b 0.0.0.0:8000 --log-file=/var/log/gunicorn.log muhal.wsgi
