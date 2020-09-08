3f21f40fb2ae148d68c203366480a9ea71f43736
Majd Al-shihabi <majd.al@gmail.com>
2020-09-08 18:10:22 -0400

    code cleanup

--- backend/config/local_settings.py
+++ backend/config/local_settings.py
@@ -9,22 +9,5 @@ DEBUG = bool(strtobool(os.environ.get('DJANGO_DEBUG', "False")))
 SECRET_KEY = os.environ['SECRET_KEY']
 NEVERCACHE_KEY = os.environ['NEVERCACHE_KEY']
 
-DATABASES = {
-    "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.sqlite3",
-        # DB name or path to database file if using sqlite3.
-        "NAME": os.path.join('data', os.environ['DB_NAME']),
-        # Not used with sqlite3.
-        "USER": "",
-        # Not used with sqlite3.
-        "PASSWORD": "",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
-    }
-}
-
 # Allowed development hosts
 ALLOWED_HOSTS = [os.environ['PROJECT_URL'], ]
